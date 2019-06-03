846a9c5c773e466407432060033e6aaaaf4113f7
Alan Bilston <18461427+GatsbyWestEgg@users.noreply.github.com>
2019-06-03 15:09:20 +1000

    Add files via upload
    Updated html code. A heading and formatting has been changed

---
+++ HBMShedWSShed_NoPw.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>
+
+Adafruit_BME280 bmp; // I2C
+
+
+const char* ssid = "SSID";
+const char* password = "password";
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+  /* Default settings from datasheet. */
+  
+
+  
+  Serial.println();
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  server.begin();
+  Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());                
+}
+
+String prepareHtmlPage()
+{
+  String htmlPage =
+     String("HTTP/1.1 200 OK\r\n") +
+            "Content-Type: text/html\r\n" +
+            "Connection: close\r\n" +  // the connection will be closed after completion of the response
+            "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
+            "\r\n" +
+            "<!DOCTYPE html>" +
+            "<html>" +
+            "<head>" +
+                "<title>HBMS Weather Station</title>" +
+            "</head>"
+            "<h1 style= color:blue>" + "Weather Station" + "</h1>" +
+            "<p>Temperature:  " + String(bmp.readTemperature()) + "  degC  </p> " 
+            "<p>Pressure:  " +String(bmp.readPressure()/100.0) + "  hPa </p>" 
+            "<p>Humidity:  " + String(bmp.readHumidity()) + " %RH </p>"
+            "</html>" +
+            "\r\n";
+  return htmlPage;
+}
+
+
+
+ void loop()
+{
+  WiFiClient client = server.available();
+  // wait for a client (web browser) to connect
+  if (client)
+  {
+    Serial.println("\n[Client connected]");
+    while (client.connected())
+    {
+      // read line by line what the client (web browser) is requesting
+      if (client.available())
+      {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+        // wait for end of client's request, that is marked with an empty line
+        if (line.length() == 1 && line[0] == '\n')
+        {
+          client.println(prepareHtmlPage());
+          break;
+        }
+      }
+    }
+    delay(1); // give the web browser time to receive the data
+
+    // close the connection:
+    client.stop();
+    Serial.println("[Client disonnected]");
+  }
+}
